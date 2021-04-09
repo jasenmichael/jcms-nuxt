8a639061eaca4743b4cd81fd0ee40a5352bcd63c
jasenmichael <jasen@ready-builder.com>
2021-04-09 16:09:57 -0500

    chore: create logout page

---
+++ pages/logout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>Logging out, Redirecting to home page..</div>
+</template>
+
+<script>
+export default {
+  // middleware: 'auth',
+  mounted() {
+    this.logout()
+  },
+  methods: {
+    logout() {
+      this.$strapi.logout()
+      // clear sessionStorage
+      sessionStorage.removeItem('adminJwtToken')
+      sessionStorage.removeItem('adminUserInfo')
+      this.$router.push('/')
+    },
+  },
+}
+</script>
