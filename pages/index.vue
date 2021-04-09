6c1c69af9885fda7e87cba161af2201fe962b3ec
jasenmichael <jasen@ready-builder.com>
2021-04-09 16:09:11 -0500

    chore: create landing page

---
+++ pages/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="grid grid-rows-1">
+    <button v-if="!$strapi.user" @click="login">login</button>
+    <button v-else @click="logout">logout</button>
+    <button>
+      <nuxt-link to="/authenticated">authenticated </nuxt-link>
+    </button>
+    <button><nuxt-link to="/admin">cms </nuxt-link></button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      email: 'jasen@jcms.io',
+      password: 'Strapi123456',
+    }
+  },
+  computed: {
+    user() {
+      return this.$strapi.user
+    },
+  },
+  methods: {
+    async login() {
+      await this.$strapi.login({
+        identifier: this.email,
+        password: this.password,
+      })
+      this.loginStrapi()
+      // this.$router.push('/authenticated')
+    },
+    logout() {
+      this.$strapi.logout()
+      this.res = ''
+      this.$router.push('/')
+    },
+    loginStrapi() {
+      fetch(`http://localhost:1337/admin/login`, {
+        // fetch(`${process.env.STRAPI_URL}/admin/login`, {
+        credentials: 'omit',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: this.email,
+          password: this.password,
+        }),
+        method: 'POST',
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          const token = res.data.token || null
+          const user = res.data.user || null
+          // eslint-disable-next-line no-console
+          console.log({
+            token,
+            user,
+          })
+          sessionStorage.setItem('adminJwtToken', JSON.stringify(token))
+          sessionStorage.setItem('adminUserInfo', JSON.stringify(user))
+        })
+    },
+  },
+}
+</script>
